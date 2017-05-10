57da9e853b3b9afe2681a4c1f2f3bee161280bcb
sarven <sar3ven112@gmail.com>
2017-05-10 23:00:33 +0200

    API - CreateMessage - send FormData instead of JSON

--- app/config/api.js
+++ app/config/api.js
@@ -3,34 +3,46 @@ const API_URL = 'front/api/';
 const MESSAGE_UPLOAD_URL = 'uploads/message/';
 const COMMENT_UPLOAD_URL = 'uploads/comment/';
 
-function createApiUrl(endpoint) {
+function createApiUrl (endpoint) {
   return BASE_URL + API_URL + endpoint;
 }
 
-export function getMessageUploadUrl(photo) {
+export function getMessageUploadUrl (photo) {
   return BASE_URL + MESSAGE_UPLOAD_URL + photo;
 }
 
-export function getCommentUploadUrl(photo) {
+export function getCommentUploadUrl (photo) {
   return BASE_URL + COMMENT_UPLOAD_URL + photo;
 }
 
-export function getMessages(lat, long) {
+export function getMessages (lat, long) {
   return fetch(createApiUrl(`message/list/${lat}/${long}`), {
     method: 'GET',
     headers: {
-      'Accept': 'application/json'
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     }
   });
 }
 
-export function createMessage(message) {
+export function createMessage (message) {
   return fetch(createApiUrl('message'), {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      'Accept': 'application/json'
     },
-    body: JSON.stringify({message: message})
+    body: createFormData('message', message)
   });
+}
+
+function createFormData (object, data) {
+  let formData = new FormData();
+
+  for(const property in data) {
+    if (data.hasOwnProperty(property) && data[property]) {
+      formData.append(`${object}[${property}]`, data[property]);
+    }
+  }
+
+  return formData;
 }
