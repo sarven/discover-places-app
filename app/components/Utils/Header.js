820f9b087e41e56a3b23a0491cb14fc1c6682738
sarven <sar3ven112@gmail.com>
2017-05-21 22:36:07 +0200

    Header - logo centered

--- app/components/Utils/Header.js
+++ app/components/Utils/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Image, Dimensions } from 'react-native';
+import { STYLES } from './../../config/style';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export const HEADER = (
   <View
-    style={{ flex: 1, marginTop: 0, justifyContent: 'center', alignSelf: 'stretch', alignItems: 'center' }}
+    style={STYLES.header}
   >
     <Image
       source={require('./../../img/logo.png')}
